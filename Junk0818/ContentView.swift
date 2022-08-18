2503be49b8822b9933146e14b693513820f8910c
Bob Wint <usa44011@yahoo.com>
2022-08-18 15:47:29 -0400

    Initial Commit

---
+++ Junk0818/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Junk0818
+//
+//  Created by Bob Wint on 8/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Junk0818/Junk0818App.swift
@@ -0,0 +1,17 @@
+//
+//  Junk0818App.swift
+//  Junk0818
+//
+//  Created by Bob Wint on 8/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Junk0818App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
