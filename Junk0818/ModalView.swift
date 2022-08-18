7beb396322cffd94acd6b5be21efbeb330920108
Bob Wint <usa44011@yahoo.com>
2022-08-18 16:05:58 -0400

    Created separate modal

---
+++ Junk0818/ModalView.swift
@@ -0,0 +1,20 @@
+//
+//  ModalView.swift
+//  Junk0818
+//
+//  Created by Bob Wint on 8/18/22.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalView()
+    }
+}
